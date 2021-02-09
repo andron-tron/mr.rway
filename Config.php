ad2b4f9539a53e894784d2ff746875b45a9c613a
Andron <andron@bmz.gomel.by>
2021-02-09 13:36:56 +0300

    вычещенны секреты

--- Config.php
+++ Config.php
@@ -2,12 +2,12 @@
 class Config {
     const MON_HOST = 'fsm:27017/MailRobots';
     const MON_USER = 'mrobot';
-    const MON_PWD =  'hoRit0wn3';
+    const MON_PWD =  '<pass>';
 
     const MSG_HOST = 'metal.bsw.iron';
     const MSG_PORT = 110;
     const MSG_USER = 'eorders';
-    const MSG_PASS = 'pmkjndfygf32es';
+    const MSG_PASS = '<pass>';
     
     // обрабатываем только сообщения от этого адреса
     const MSG_FROM = 'poisk_vagon@mnsk.rw.by';
