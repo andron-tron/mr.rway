8cd1ac9808b8519ef81db65e5d223dd83cc3a73a
andron-tron <77525022+andron-tron@users.noreply.github.com>
2021-06-08 16:14:54 +0300

    cleanup

--- Config.php
+++ Config.php
@@ -7,11 +7,11 @@ class Config {
     const MSG_HOST = 'metal.bsw.iron';
     const MSG_PORT = 110;
     const MSG_USER = 'eorders';
-    const MSG_PASS = 'pmkjndfygf32es';
+    const MSG_PASS = '<pass>';
     
     // обрабатываем только сообщения от этого адреса
-    // const MSG_FROM = 'poisk_vagon@mnsk.rw.by';
+    const MSG_FROM = 'poisk_vagon@mnsk.rw.by';
     const NUM_ATTEMPTS = 5;
-    const RE_TIME = 10; //second
-    const MSG_FROM = 'ag.novikov@bmz.gomel.by';
+    const RE_TIME = 600; //second
+    //const MSG_FROM = 'ag.novikov@bmz.gomel.by';
 }
